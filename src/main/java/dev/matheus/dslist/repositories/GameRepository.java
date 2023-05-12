078cff1926c5c83cf4acb0837a9cdd70064b2784
Matheus H. Passos <matheus.passos@ifgoiano.edu.br>
2023-05-12 09:00:23 -0300

    feat: games by list ordered by position

--- src/main/java/dev/matheus/dslist/repositories/GameRepository.java
+++ src/main/java/dev/matheus/dslist/repositories/GameRepository.java
@@ -3,10 +3,12 @@ package dev.matheus.dslist.repositories;
 import dev.matheus.dslist.entities.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 
 import java.util.List;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
-    @Query("SELECT g FROM Game g INNER JOIN Belonging b ON b.belongingPK.game.id = g.id WHERE b.belongingPK.gameList.id = :listId")
-    List<Game> findGamesInList(Long listId);
+    @Query("SELECT g FROM Game g INNER JOIN Belonging b ON b.belongingPK.game.id = g.id WHERE b.belongingPK.gameList.id = :listId ORDER BY b.position ASC")
+    List<Game> findGamesInList(@Param("listId") Long listId);
 }
